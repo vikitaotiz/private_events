bb3195af8067405ab60735ff6f439507d45da604
Victor Otieno <victorotieno598@gmail.com>
2021-06-18 17:53:42 +0300

    Fix linter error

--- app/controllers/attended_events_controller.rb
+++ app/controllers/attended_events_controller.rb
@@ -3,7 +3,7 @@ class AttendedEventsController < ApplicationController
     event = Event.find(params[:event_id])
     @attend = AttendedEvent.where('event_id = ?', event.id)
   end
-  
+
   def create
     event = Event.find(params[:event_id])
     @attended_event = AttendedEvent.new(event_id: event.id, user_id: current_user.id)
