b4bb0524c426eef2feba02949141799ccfedf0c6
Victor Otieno <victorotieno598@gmail.com>
2021-06-18 14:50:11 +0300

    Add event model and controller

---
+++ db/migrate/20210618114913_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.date :date
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
