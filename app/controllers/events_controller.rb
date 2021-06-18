c707747ec594b1dc867d5bfe3bf23ea3fdcb4993
Victor Otieno <victorotieno598@gmail.com>
2021-06-18 15:23:25 +0300

    Add new and create methods, add new in the views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,26 @@ class EventsController < ApplicationController
     # @previous_events = Event.previous
     # @upcoming_events = Event.upcoming
   end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :location, :user_id)
+  end
 end
