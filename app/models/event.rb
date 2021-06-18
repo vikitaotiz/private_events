fb2ffd1b1dcbaf4d07f51377c0ebfd3fa6e927fd
Victor Otieno <victorotieno598@gmail.com>
2021-06-18 15:13:23 +0300

    Add associations in the attended_event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
     belongs_to :user
+    has_many :attended_events
+    
+    validates :user, presence: true
+    validates :date, presence: true
+    validates :location, presence: true
 end
