99cd87765aef6c0602caab42021d2698499a7828
Victor Otieno <victorotieno598@gmail.com>
2021-06-18 15:18:01 +0300

    Edit index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,10 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
+    @past_events = Event.all
+    @upcoming_events = Event.all
+
+    # @previous_events = Event.previous
+    # @upcoming_events = Event.upcoming
   end
 end
