3d523ade307e455713e6b56d56e364b099a04854
Victor Otieno <victorotieno598@gmail.com>
2021-06-18 15:10:19 +0300

    Edit migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_114913) do
+ActiveRecord::Schema.define(version: 2021_06_18_120555) do
+
+  create_table "attended_events", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_attended_events_on_event_id"
+    t.index ["user_id"], name: "index_attended_events_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +27,8 @@ ActiveRecord::Schema.define(version: 2021_06_18_114913) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +43,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_114913) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attended_events", "events"
+  add_foreign_key "attended_events", "users"
+  add_foreign_key "events", "users"
 end
